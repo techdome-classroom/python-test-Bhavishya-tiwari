b6058f639a0f8ce7ed0deaa5cabd4f0911323234
bhavishya-tiwari <bhavishyatiwari2000@gmail.com>
2024-05-08 10:42:00 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,6 +2,6 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-        
+        print(grid[1][3])
                     
         return 0
