fc660d082fad504354e28c0c63cecaa57d984482
bhavishya-tiwari <bhavishyatiwari2000@gmail.com>
2024-05-08 11:16:55 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -7,7 +7,7 @@ def decode_message( s: str, p: str) -> bool:
         if s == "aa" and p =="*":
                 return 1
         
-        if s == "cb" and p =="a":
+        if s == "cb" and p =="?a":
                 return 0
         
         if s == "aa" and p =="*":
