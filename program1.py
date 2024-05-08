7e7ce3573024e9c72d184e5a02f6361fa1c6053b
bhavishya-tiwari <bhavishyatiwari2000@gmail.com>
2024-05-08 10:56:28 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -15,7 +15,7 @@ class Solution:
         if chk(g,i+1,j)==1:
             dfs(g,i+1,j)
         
-        if chk(g,i+1,j+1)==1:
+        if chk(g,i,j+1)==1:
             dfs(g,i+1,j+1)
         
         if chk(g,i,j+1)==1:
