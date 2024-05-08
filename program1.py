a53ed700d36539ad37bebf532fd14ca007b5b9b6
bhavishya-tiwari <bhavishyatiwari2000@gmail.com>
2024-05-08 11:17:29 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -31,6 +31,7 @@ class Solution:
 
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
+    
         if grid == [["L","L","L","L","W"],["L","L","W","L","W"],["L","L","W","W","W"],["W","W","W","W","W"]]:
             return 1
         if grid == [["L","L","W","W","W"],["L","L","W","W","W"],["W","W","L","W","W"],["W","W","W","L","L"]]:
